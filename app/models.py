cac348e885197c03617e5f9f6283898c40b3c481
Gabriel Lorenzo Bernini Bondarev <gabrielbernini06@gmail.com>
2025-12-12 10:04:30 +0100

    Add Card model and update relationships
    Added Card model with relationships to List and Board.

--- app/models.py
+++ app/models.py
@@ -1,47 +1,49 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship
 from .database import Base
 
-#Creación de las clases (heredan de Base)
 class User(Base):
-    __tablename__ = "users" #Nombre de la tabla
+    __tablename__ = "users"
     
-    #Definición de las columnas
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default=True) #Para desactivar usuarios sin borrarlos
+    is_active = Column(Boolean, default=True)
 
-    #Relación one - to - many con boards
     boards = relationship("Board", back_populates="owner")
-
-
 
 class Board(Base):
     __tablename__ = "boards"
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
-    
-    #Relación con la tabla users
     owner_id = Column(Integer, ForeignKey("users.id"))
 
-    #Relación one - to - many con users
     owner = relationship("User", back_populates="boards")
-
-    #Relación one - to - many con lists
     lists = relationship("List", back_populates="board")
-
 
 class List(Base): 
     __tablename__ = "lists" 
 
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
-    position = Column(Integer)  #para saber en que orden mostrarlas
-    
-    #Relación con la tabla boards
+    position = Column(Integer)
     board_id = Column(Integer, ForeignKey("boards.id"))
 
-    #Relación one - to - many con boards
     board = relationship("Board", back_populates="lists")
+    # Relación con cards
+    cards = relationship("Card", back_populates="list", cascade="all, delete-orphan")
+
+class Card(Base):
+    __tablename__ = "cards"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, nullable=True)
+    deadline = Column(DateTime, nullable=True)
+    
+    list_id = Column(Integer, ForeignKey("lists.id"))
+    board_id = Column(Integer, ForeignKey("boards.id")) 
+
+    list = relationship("List", back_populates="cards")
+    board = relationship("Board")
